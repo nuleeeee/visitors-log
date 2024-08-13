fc9fb4f9a38c0923b573e34f16b1e3f37aaa7fb4
Noli Briones <124874418+nuleeeee@users.noreply.github.com>
2024-08-13 13:45:18 +0800

    Update connect.php

--- php/connect.php
+++ php/connect.php
@@ -1,8 +1,8 @@
 <?php
 $settings = array();
 $counter = 0;
-define('DB_SERVER', "192.168.1.124:3306");
-define('DB_USERNAME', 'hoq1');
-define('DB_PASSWORD', 'gmanquiil');
+define('DB_SERVER', "localhost");
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'password');
 $db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD);
 ?>
