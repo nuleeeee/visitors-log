ca43337366f3d674ac995a14da93f6c7982a850f
Noli Briones <nolsatwork@gmail.com>
2024-10-04 15:36:09 +0800

    past updates

--- php/connect.php
+++ php/connect.php
@@ -1,8 +1,7 @@
 <?php
 $settings = array();
 $counter = 0;
-define('DB_SERVER', "localhost");
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'password');
+define('DB_SERVER', "192.168.1.124");
+define('DB_USERNAME', 'hoq1');
+define('DB_PASSWORD', 'gmanquiil');
 $db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD);
-?>
